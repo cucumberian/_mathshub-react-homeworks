bc6d79bb9637cf5e7e053e5b1d1e256b8bd5c910
cucumberian <smamarsello@gmail.com>
2023-07-31 12:41:36 +0300

    fix: changed user profile to Fox Mulder

--- my-profile/src/components/user.js
+++ my-profile/src/components/user.js
@@ -1,15 +1,16 @@
 const user = {
-  avatar_url: "https://avatars.githubusercontent.com/u/65560085",
-  name: "Михаил",
-  bio: "Обычный человек. Старый, не не бесполезный.",
+  avatar_url:
+    "//external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.YEm8ILlojX78PXuLOVst9QHaJQ%26pid%3DApi&f=1&ipt=e3861016c3d6a0fc27089536fd0db7db6f81cc27782e32df2cb991b8e6065e6f&ipo=images",
+  name: "Фокс Маудер",
+  bio: "В детстве его сестра Саманта таинственно исчезла, с тех пор он её ищет. Окончил Оксфорд с отличием, затем академию ФБР в Квантико. Работает в ФБР в отделе Секретных материалов.",
   citation: {
-    text: "I want to beleave",
-    author: "Fox Mulder",
+    text: "Истина где-то рядом",
+    author: "Шерлок Холмс",
   },
   hobbies: [
-    { id: 0, name: "Ролики" },
-    { id: 1, name: "Велосипед" },
-    { id: 2, name: "Аниме" },
+    { id: 0, name: "Теории заговоров" },
+    { id: 1, name: "Фантастические сериалы" },
+    { id: 2, name: "Порнография" },
   ],
 };
 
